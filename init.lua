d767c6d8781b09597731dcdba1c544c761bb0b23
adriannic <spamadriannic@gmail.com>
2023-04-30 17:22:45 +0200

    Format

--- init.lua
+++ init.lua
@@ -1,49 +1,46 @@
 return {
   -- Configure AstroNvim updates
   updater = {
-    remote = "origin", -- remote to use
-    channel = "stable", -- "stable" or "nightly"
-    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "nightly", -- branch name (NIGHTLY ONLY)
-    commit = nil, -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false, -- skip prompts about breaking changes
-    show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false, -- automatically quit the current session after a successful update
-    remotes = { -- easily add new remotes to track
+    remote = "origin",         -- remote to use
+    channel = "stable",        -- "stable" or "nightly"
+    version = "latest",        -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    branch = "nightly",        -- branch name (NIGHTLY ONLY)
+    commit = nil,              -- commit hash (NIGHTLY ONLY)
+    pin_plugins = nil,         -- nil, true, false (nil will pin plugins on stable only)
+    skip_prompts = false,      -- skip prompts about breaking changes
+    show_changelog = true,     -- show the changelog after performing an update
+    auto_quit = false,         -- automatically quit the current session after a successful update
+    remotes = {                -- easily add new remotes to track
       --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
       --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
-    },
+    }
   },
 
   -- Set colorscheme to use
   colorscheme = "astrodark",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-  diagnostics = {
-    virtual_text = true,
-    underline = true,
-  },
+  diagnostics = { virtual_text = true, underline = true },
 
   lsp = {
     -- customize lsp formatting options
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true, -- enable or disable format on save globally
-        allow_filetypes = { -- enable format on save for specified filetypes only
+        enabled = false,            -- enable or disable format on save globally
+        allow_filetypes = {         -- enable format on save for specified filetypes only
           -- "go",
         },
-        ignore_filetypes = { -- disable format on save for specified filetypes
+        ignore_filetypes = {         -- disable format on save for specified filetypes
           -- "python",
-        },
+        }
       },
-      disabled = { -- disable formatting capabilities for the listed language servers
+      disabled = {       -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
       },
-      timeout_ms = 1000, -- default format timeout
+      timeout_ms = 1000       -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
@@ -51,7 +48,7 @@ return {
     -- enable servers that you already have installed without mason
     servers = {
       -- "pyright"
-    },
+    }
   },
 
   -- Configure require("lazy").setup() options
@@ -60,9 +57,12 @@ return {
     performance = {
       rtp = {
         -- customize default disabled vim plugins
-        disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin" },
-      },
-    },
+        disabled_plugins = {
+          "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin",
+          "tarPlugin"
+        }
+      }
+    }
   },
 
   -- This function is run last and is a good place to configuring
@@ -81,5 +81,5 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
-  end,
+  end
 }
