5ef1b6389af4ab3f8fd631e796606633c606769a
adriannic <spamadriannic@gmail.com>
2023-04-30 17:25:25 +0200

    Add clangd_extensions plugin

---
+++ plugins/clangd_extensions.lua
@@ -0,0 +1,18 @@
+return {
+    lsp = {
+        setup_handlers = {
+            -- add custom handler
+            clangd = function(_, opts)
+                require("clangd_extensions").setup {server = opts}
+            end
+        },
+        config = {clangd = {capabilities = {offsetEncoding = "utf-8"}}}
+    },
+    "p00f/clangd_extensions.nvim", -- install lsp plugin
+    {
+        "williamboman/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = {"clangd"} -- automatically install lsp
+        }
+    }
+}
