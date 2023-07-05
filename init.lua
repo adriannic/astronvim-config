de4fd5d46d1634cc22f86000b6a275da1129d152
adriannic <spamadriannic@gmail.com>
2023-07-05 15:37:35 +0200

    format and disable default virtual text as its redundant with lsp-lines installed

--- init.lua
+++ init.lua
@@ -5,23 +5,27 @@ return {
         channel = "stable", -- "stable" or "nightly"
         version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
         branch = "nightly", -- branch name (NIGHTLY ONLY)
-        commit = nil, -- commit hash (NIGHTLY ONLY)
+        commit = nil,      -- commit hash (NIGHTLY ONLY)
         pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
         skip_prompts = false, -- skip prompts about breaking changes
         show_changelog = true, -- show the changelog after performing an update
         auto_quit = false, -- automatically quit the current session after a successful update
-        remotes = { -- easily add new remotes to track
+        remotes = {        -- easily add new remotes to track
             --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
             --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
             --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
-        }
+        },
     },
 
     -- Set colorscheme to use
     colorscheme = "catppuccin",
 
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {virtual_text = true, underline = true},
+    diagnostics = {
+        virtual_text = false,
+        virtual_lines = true,
+        underline = true,
+    },
 
     lsp = {
         -- customize lsp formatting options
@@ -34,13 +38,13 @@ return {
                 },
                 ignore_filetypes = { -- disable format on save for specified filetypes
                     -- "python",
-                }
+                },
             },
             disabled = { -- disable formatting capabilities for the listed language servers
                 -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
                 -- "lua_ls",
             },
-            timeout_ms = 1000 -- default format timeout
+            timeout_ms = 1000, -- default format timeout
             -- filter = function(client) -- fully override the default formatting function
             --   return true
             -- end
@@ -49,21 +53,25 @@ return {
         servers = {
             -- "pyright"
         },
-        config = {clangd = {capabilities = {offsetEncoding = "utf-8"}}}
+        config = { clangd = { capabilities = { offsetEncoding = "utf-8" } } },
     },
 
     -- Configure require("lazy").setup() options
     lazy = {
-        defaults = {lazy = true},
+        defaults = { lazy = true },
         performance = {
             rtp = {
                 -- customize default disabled vim plugins
                 disabled_plugins = {
-                    "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin",
-                    "tarPlugin"
-                }
-            }
-        }
+                    "tohtml",
+                    "gzip",
+                    "matchit",
+                    "zipPlugin",
+                    "netrwPlugin",
+                    "tarPlugin",
+                },
+            },
+        },
     },
 
     -- This function is run last and is a good place to configuring
@@ -82,5 +90,5 @@ return {
         --     ["~/%.config/foo/.*"] = "fooscript",
         --   },
         -- }
-    end
+    end,
 }
