378fc6506f2198f3413d7f613d380f3aff73d631
adriannic <spamadriannic@gmail.com>
2024-03-06 10:21:16 +0100

    Add nix formatter

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -18,6 +18,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       if not opts.ensure_installed then opts.ensure_installed = {} end
       require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "nixpkgs-fmt",
         "sonarlint-language-server",
       })
     end,
